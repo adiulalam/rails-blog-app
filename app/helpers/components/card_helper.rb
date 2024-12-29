9b1cd66a17d8ec976ed87e3d0b66c56fa8db8b48
Adiul Alam Adil <adiulalam@gmail.com>
2024-12-29 09:05:14 +0000

    Add button component

--- app/helpers/components/card_helper.rb
+++ app/helpers/components/card_helper.rb
@@ -1,5 +1,13 @@
 module Components::CardHelper
-  def render_card(title: nil, subtitle: nil, body: nil, footer: nil, **options, &block)
-    render "components/ui/card", title: title, subtitle: subtitle, footer: footer, body: (block ? capture(&block) : body), block:, options: options
+  def render_card(title: nil, subtitle: nil, body: nil, footer: nil, link: nil, **options, &block)
+    title_content = link.present? ? link_to(title, link, class: "hover:underline hover:text-primary/80") : title
+
+    render "components/ui/card",
+                   title: title_content,
+                   subtitle: subtitle,
+                   footer: footer,
+                   body: (block ? capture(&block) : body),
+                   block:,
+                   options: options
   end
 end
